cdd2c7c4aad0b98e200bba03fdc7beeebda272fc
Viktor Voronin <mobimrzg@gmail.com>
2018-02-08 17:25:24 +0500

    add eventStateUpdaters
    Module includes state updaters for Event component
    Implemented setFormFieldsErrors()

---
+++ src/componets/Event/eventStateUpdaters.js
@@ -0,0 +1,9 @@
+export const setFormFieldsErrors = fields => form => {
+  const errorFields = fields.reduce((acc, fieldName) => {
+    return {
+      ...acc,
+      [fieldName]: { ...form[fieldName], errors: true }
+    };
+  }, {});
+  return { ...form, ...errorFields };
+};
